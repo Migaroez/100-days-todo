46583f15329032be9dea4a7527c0f16fa246b34a
Migaroez <geusens@gmail.com>
2021-09-09 12:56:38 +0200

    [4] First pass on c# api

---
+++ C# Api/ApiEntityFrameworkMemory/Controllers/ItemController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Todo.Domain;
+using Todo.Repositories;
+using Todo.Repositories.Filters;
+
+namespace ApiEntityFrameworkMemory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly ILogger<ItemController> _logger;
+        private readonly IItemRepository _itemRepository;
+
+        public ItemController(ILogger<ItemController> logger, IItemRepository itemRepository)
+        {
+            _logger = logger;
+            _itemRepository = itemRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> Get(bool archivedOnly = false, bool unarchivedOnly = false, Guid? noteId = null)
+        {
+            return _itemRepository.GetFiltered(new ItemFilter
+            {
+                ArchivedOnly = archivedOnly,
+                NoteId = noteId,
+                UnArchivedOnly = unarchivedOnly
+            });
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Item Get(Guid id)
+        {
+            return _itemRepository.Get(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post(string description)
+        {
+            var newItem = _itemRepository.Upsert(new Item
+            {
+                Description = description,
+                CreateDate = DateTimeOffset.Now
+            });
+
+            if (newItem == null)
+            {
+                return Problem("Could not create item in repository");
+            }
+
+            return Created("/ItemController/" + newItem.Id, newItem);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put(Guid id, string description)
+        {
+            var existingItem = _itemRepository.Get(id);
+            if (existingItem == null)
+            {
+                return NotFound(id);
+            }
+
+            existingItem.Description = description;
+            var updatedItem = _itemRepository.Upsert(existingItem);
+
+            return Ok(updatedItem);
+        }
+
+        [HttpPut]
+        [Route("{id}/ToggleComplete")]
+        public IActionResult ToggleComplete(Guid id)
+        {
+            var existingItem = _itemRepository.Get(id);
+            if (existingItem == null)
+            {
+                return NotFound(id);
+            }
+
+            existingItem.CompleteDate = existingItem.CompleteDate != null ? DateTimeOffset.Now : null;
+            var updatedItem = _itemRepository.Upsert(existingItem);
+
+            return Ok(updatedItem);
+        }
+
+        [HttpPut]
+        [Route("{id}/ToggleArchived")]
+        public IActionResult ToggleArchived(Guid id)
+        {
+            var existingItem = _itemRepository.Get(id);
+            if (existingItem == null)
+            {
+                return NotFound(id);
+            }
+
+            existingItem.ArchiveDate = existingItem.ArchiveDate != null ? DateTimeOffset.Now : null;
+            var updatedItem = _itemRepository.Upsert(existingItem);
+
+            return Ok(updatedItem);
+        }
+
+        [HttpPost]
+        [Route("{id}/Note")]
+        public IActionResult Note(Guid id, string content)
+        {
+            var existingItem = _itemRepository.Get(id);
+            if (existingItem == null)
+            {
+                return NotFound(id);
+            }
+
+            var now = DateTimeOffset.Now;
+
+            existingItem.Notes.Add(new Note
+            {
+                CreateDate = now,
+                Content = content
+            });
+
+            var updatedItem = _itemRepository.Upsert(existingItem);
+            var lastNote = updatedItem.Notes.SingleOrDefault(n => n.CreateDate == now);
+            if (lastNote == null)
+            {
+                return Problem("Could not persist note in repository");
+            }
+
+            return Created("/Note/" + lastNote.Id, lastNote);
+        }
+
+        [HttpGet]
+        [Route("/Note/{id}")]
+        public Note GetNote(Guid id)
+        {
+            var item =  _itemRepository.GetFiltered(new ItemFilter
+            {
+                NoteId = id
+            }).SingleOrDefault();
+
+            return item?.Notes.FirstOrDefault(n => n.Id == id);
+        }
+
+        [HttpPut]
+        [Route("/Note/{id}")]
+        public IActionResult PutNote(Guid id, string content)
+        {
+            var item = _itemRepository.GetFiltered(new ItemFilter
+            {
+                NoteId = id
+            }).SingleOrDefault();
+
+            var note = item?.Notes.FirstOrDefault(n => n.Id == id);
+            if (note == null)
+            {
+                return NotFound(id);
+            }
+
+            note.Content = content;
+            var updatedItem = _itemRepository.Upsert(item);
+            return Ok(updatedItem.Notes.FirstOrDefault(n => n.Id == id));
+        }
+
+        [HttpDelete]
+        [Route("/Note/{id}")]
+        public IActionResult DeleteNote(Guid id)
+        {
+            var item = _itemRepository.GetFiltered(new ItemFilter
+            {
+                NoteId = id
+            }).SingleOrDefault();
+
+            var note = item?.Notes.FirstOrDefault(n => n.Id == id);
+            if (note == null)
+            {
+                return NotFound(id);
+            }
+
+            item.Notes.Remove(note);
+            _itemRepository.Upsert(item);
+
+            return Ok();
+        }
+    }
+}
